869e02ae4f33fb407b600a2a75fe3711b29115f8
Vovashprot <vova.protasenko@gmail.com>
2023-05-03 14:30:52 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
+    public static void main(String[] args) {
+        List<Integer> dudes = new ArrayList<>(List.of(4,5,53,6,2));
+        System.out.println(Service.isEven(dudes));
     }
 }
--- src/Service.java
+++ src/Service.java
@@ -2,5 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
-
+    public static List<Integer> isEven(List<Integer> input){
+        List<Integer> output = new ArrayList<>(List.of());
+        for (int i = 0; i < input.size(); i++){
+            if (input.get(i) % 2 == 0){
+                output.add(input.get(i));
+            }
+        }
+        return output;
+    }
 }
