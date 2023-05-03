84e7af6e89fc6eddee46ffd269d642c8465dea36
Vovashprot <vova.protasenko@gmail.com>
2023-05-03 15:35:37 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> dudes = new ArrayList<>(List.of(4,5,53,6,2));
-        System.out.println(Service.isEven(dudes));
+        List<String> words = new ArrayList<>(List.of("один", "один", "два","два", "два"));
+        System.out.println(Service.isOdd(dudes));
+        System.out.println(Service.isEvenAndIncr(dudes));
+        System.out.println(Service.uniqueWords(words));
     }
 }
--- src/Service.java
+++ src/Service.java
@@ -1,13 +1,31 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Service {
-    public static List<Integer> isEven(List<Integer> input){
+    public static List<Integer> isOdd(List<Integer> input){
         List<Integer> output = new ArrayList<>(List.of());
+        for (int i = 0; i < input.size(); i++){
+            if (input.get(i) % 2 != 0){
+                output.add(input.get(i));
+            }
+        }
+        return output;
+    }
+    public static Set<Integer> isEvenAndIncr(List<Integer> input){
+        Set<Integer> output = new HashSet<>(List.of());
         for (int i = 0; i < input.size(); i++){
             if (input.get(i) % 2 == 0){
                 output.add(input.get(i));
             }
+        }
+        return output;
+    }
+    public static Set<String> uniqueWords(List<String> input){
+        Set<String> output = new HashSet<>(List.of());
+        for (int i = 0; i < input.size(); i++){
+                output.add(input.get(i));
         }
         return output;
     }
